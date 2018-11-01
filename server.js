2c7f1f171ec29e427d872ca6365e7baac0129bb6
Emanafi <elpi.manafi@gmail.com>
2018-11-01 08:03:39 -0700

    allow cross origin reqs, set up root route

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const app = express();
 
+//parse incoming url encoded form data
+//and populate the req.body object
+
+app.use(express.json());
+
+//allow cross origin requests
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use( (req, res, next) => {
+	res.header("Access_Control_Allow_Origin", "*");
+	res.header("Access_Control_Allow_Headers", "Origin, x-requested-With, Content-Type, Accept");
+	next();
+});
+
+/**************
+*   DATABASE  *
+***************/
+const db = require('./models/index.js');
+
+
+/**************
+*    ROUTES   *
+***************/
+
+//Serve static files from the '/public' directory:
+//i.e. '/images', '/scripts', '/styles'
+app.use(express.static('public'));
+
+// HTML Endpoints
 app.get('/', (req, res) => {
-	res.send('this will be the landing page soon');
+	res.sendFile(__dirNname + 'views/landingpage.html');
 })
 
 
