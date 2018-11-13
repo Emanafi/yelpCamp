c8ff57953c99e904f3b74ca4a2b65ef8e953bb5e
Emanafi <elpi.manafi@gmail.com>
2018-11-13 08:31:29 -0800

    updates to app.js file

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	
+	//global variables
+	const searchItem = document.getElementById('filter-submit');
+	const submitButton = document.getElementById('search');
+	const baseURL = 'http://localhost:3000';
+
+	//get 
+
+
+	//event listeners
+	submitButton.addEventListener('submit', /*callbackfunction*/)
+});
